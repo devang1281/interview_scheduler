86e9ee19b3cb340a0d05bf1a0ef8243d54086d8a
Devang  Bansal <INTERNAL\devang.b@Desk-Devang.internal.talview.com>
2020-01-31 15:14:05 +0530

    Commit -2 before branching

---
+++ scheduling/candidate_panelist.js
@@ -0,0 +1,7 @@
+function common_time(panelist,candidate) {
+    console.log(candidate[0].timeInUTC[1].length)
+    console.log(47/2)
+    // console.log(panelist[0],panelist[1],candidate[0])
+}
+
+module.exports = common_time;
